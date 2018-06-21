a16c6d368dce76cb52dca2a192bf3843005e0dc7
ichiro.kirameki <ec2-user@ip-172-31-29-145.us-east-2.compute.internal>
2018-06-21 01:30:11 +0000

    want?

--- resources/views/items/have_button.blade.php
+++ resources/views/items/have_button.blade.php
@@ -1,7 +1,7 @@
 @if (Auth::user()->is_having($item->code))
     {!! Form::open(['route' => 'item_user.dont_have', 'method' => 'delete']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
-        {!! Form::submit('Have', ['class' => 'btn btn-success']) !!}
+        {!! Form::submit('Have?', ['class' => 'btn btn-success']) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => 'item_user.have']) !!}
